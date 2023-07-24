09e1440a3310ef4227f284bd953bb69c3e60f47c
braian romero <romerobraian1010@gmail.com>
2023-07-24 05:01:26 -0300

    se agrego el archivo casas.js y se modifico codigo.js y el html

---
+++ casas.js
@@ -0,0 +1,78 @@
+const casaNorte =[{
+    tipo : "Casa estandar",
+    agregado : "c/pileta",
+    precio : 80000,
+    zona: "Zona norte"
+},
+{
+    tipo : "Casa de dos pisos",
+    agregado : "c/pileta",
+    precio : 160000,
+    zona: "Zona norte"
+},
+{
+    tipo : "Casa estandar",
+    agregado : "s/pileta",
+    precio : 45000,
+    zona: "Zona norte"
+}
+]
+// ----------------------------
+const casaSur =[{
+    tipo : "Casa de dos pisos",
+    agregado : "s/pileta",
+    precio : 139000,
+    zona: "Zona sur"
+},
+{
+    tipo : "Casa estandar",
+    agregado : "c/pileta",
+    precio : 85000,
+    zona: "Zona sur"
+
+},
+{
+    tipo : "Casa de dos pisos",
+    agregado : "s/pileta",
+    precio : 69000,
+    zona: "Zona sur"
+
+}]
+// ----------------------------
+const casaEste =[{
+    tipo : "Casa de cuatro pisos",
+    agregado : "s/pileta",
+    precio : 210000,
+    zona: "Zona Este"
+},
+{
+    tipo : "Casa de dos pisos",
+    agregado : "s/pileta",
+    precio : 140000,
+    zona: "Zona Este"
+},
+{
+    tipo : "Casa estandar",
+    agregado : "c/pileta",
+    precio : 67000,
+    zona: "Zona Este"
+}]
+// ---------------------------------------
+const casaOeste =[{
+    tipo : "Casa estandar",
+    agregado : "s/pileta",
+    precio : 70000,
+    zona: "Zona Oeste"
+},
+{
+    tipo : "Casa de dos pisos",
+    agregado : "c/pileta",
+    precio : 160000,
+    zona: "Zona Oeste"
+},
+{
+    tipo : "Casa de dos pisos",
+    agregado : "s/pileta",
+    precio : 138000,
+    zona: "Zona Oeste"
+}]
