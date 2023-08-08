65d17688a8c37b9b037c91e51db0ca87a29a55d1
braian romero <romerobraian1010@gmail.com>
2023-08-07 23:23:39 -0300

    se modifico el index.html, casas.js y codigo.js

--- casas.js
+++ casas.js
@@ -1,78 +1,89 @@
-const casaNorte =[{
-    tipo : "Casa estandar",
-    agregado : "c/pileta",
-    precio : 80000,
-    zona: "Zona norte"
-},
-{
-    tipo : "Casa de dos pisos",
-    agregado : "c/pileta",
-    precio : 160000,
-    zona: "Zona norte"
-},
-{
-    tipo : "Casa estandar",
-    agregado : "s/pileta",
-    precio : 45000,
-    zona: "Zona norte"
+const arrayPrecios = [];
+let casasArray =  [];
+
+class casaNorte{
+    constructor(id, tipo, agregado, precio, zona){
+        this.id = id;
+        this.tipo = tipo;
+        this.agregado = agregado;
+        this.precio = precio;
+        this.zona = zona
+    }
 }
-]
-// ----------------------------
-const casaSur =[{
-    tipo : "Casa de dos pisos",
-    agregado : "s/pileta",
-    precio : 139000,
-    zona: "Zona sur"
-},
-{
-    tipo : "Casa estandar",
-    agregado : "c/pileta",
-    precio : 85000,
-    zona: "Zona sur"
 
-},
-{
-    tipo : "Casa de dos pisos",
-    agregado : "s/pileta",
-    precio : 69000,
-    zona: "Zona sur"
 
-}]
+const casaNorte1 = new casaNorte (1, "Casa estandar", "C/pileta", 80000, "Zona norte");
+const casaNorte2 = new casaNorte (2, "Casa de dos pisos", "C/pileta", 160000, "Zona norte");
+const casaNorte3 = new casaNorte (3, "Casa estandar", "S/pileta", 45000, "Zona norte");
+const casaNorte4 = new casaNorte (4, "Casa de tres pisos", "S/pileta", 170000, "Zona norte");
+const casaNorte5 = new casaNorte (5, "Casa de dos pisos", "S/pileta", 130000, "Zona norte");
+
+casasArray.push(casaNorte1, casaNorte2, casaNorte3, casaNorte4, casaNorte5);
+
+arrayPrecios.push(casaNorte1.precio, casaNorte2.precio, casaNorte3.precio, casaNorte4.precio, casaNorte5.precio);
+
+
 // ----------------------------
-const casaEste =[{
-    tipo : "Casa de cuatro pisos",
-    agregado : "s/pileta",
-    precio : 210000,
-    zona: "Zona Este"
-},
-{
-    tipo : "Casa de dos pisos",
-    agregado : "s/pileta",
-    precio : 140000,
-    zona: "Zona Este"
-},
-{
-    tipo : "Casa estandar",
-    agregado : "c/pileta",
-    precio : 67000,
-    zona: "Zona Este"
-}]
+class casaSur{
+    constructor(id, tipo, agregado, precio, zona){
+        this.id = id;
+        this.tipo = tipo;
+        this.agregado = agregado;
+        this.precio = precio;
+        this.zona = zona
+    }
+}
+
+const casaSur1 = new casaSur (6, "Casa de dos pisos", "S/pileta", 139000, "Zona sur");
+const casaSur2 = new casaSur (7, "Casa estandar", "C/pileta", 85000, "Zona sur");
+const casaSur3 = new casaSur (8, "Casa de dos pisos", "C/pileta", 180000, "Zona sur");
+const casaSur4 = new casaSur (9, "Casa de tres pisos", "S/pileta", 156000, "Zona sur");
+const casaSur5 = new casaSur (10, "Casa estandar", "S/pileta", 68000, "Zona sur");
+
+casasArray.push(casaSur1, casaSur2, casaSur3, casaSur4, casaSur5);
+
+arrayPrecios.push(casaSur1.precio, casaSur2.precio, casaSur3.precio, casaSur4.precio, casaSur5.precio);
+
+// ----------------------------
+class casaEste{
+    constructor(id, tipo, agregado, precio, zona){
+        this.id = id;
+        this.tipo = tipo;
+        this.agregado = agregado;
+        this.precio = precio;
+        this.zona = zona
+    }
+}
+
+const casaEste1 = new casaEste (11, "Casa de cuatro pisos", "S/pileta", 210000, "Zona este");
+const casaEste2 = new casaEste (12, "Casa de dos pisos", "S/pileta", 140000, "Zona este");
+const casaEste3 = new casaEste (13, "Casa estandar", "C/pileta", 67000, "Zona este");
+const casaEste4 = new casaEste (14, "Casa de dos pisos", "C/pileta", 175000, "Zona este");
+const casaEste5 = new casaEste (15, "Casa estandar", "S/pileta", 56000, "Zona este");
+
+casasArray.push(casaEste1, casaEste2, casaEste3, casaEste4, casaEste5);
+
+arrayPrecios.push(casaEste1.precio, casaEste2.precio, casaEste3.precio, casaEste4.precio, casaEste5.precio);
+
 // ---------------------------------------
-const casaOeste =[{
-    tipo : "Casa estandar",
-    agregado : "s/pileta",
-    precio : 70000,
-    zona: "Zona Oeste"
-},
-{
-    tipo : "Casa de dos pisos",
-    agregado : "c/pileta",
-    precio : 160000,
-    zona: "Zona Oeste"
-},
-{
-    tipo : "Casa de dos pisos",
-    agregado : "s/pileta",
-    precio : 138000,
-    zona: "Zona Oeste"
-}]
+class casaOeste{
+    constructor(id, tipo, agregado, precio, zona){
+        this.id = id;
+        this.tipo = tipo;
+        this.agregado = agregado;
+        this.precio = precio;
+        this.zona = zona
+    }
+}
+
+const casaOeste1 = new casaOeste (16, "Casa estandar", "S/pileta", 70000, "Zona Oeste");
+const casaOeste2 = new casaOeste (17, "Casa de dos pisos", "C/pileta", 130000, "Zona Oeste");
+const casaOeste3 = new casaOeste (18, "Casa estandar", "C/pileta", 85000, "Zona Oeste");
+const casaOeste4 = new casaOeste (19, "Casa de dos pisos", "S/pileta", 138000, "Zona Oeste");
+const casaOeste5 = new casaOeste (20, "Casa de tres pisos", "C/pileta", 145000, "Zona Oeste");
+
+casasArray.push(casaOeste1, casaOeste2, casaOeste3, casaOeste4, casaOeste5);
+
+arrayPrecios.push(casaOeste1.precio, casaOeste2.precio, casaOeste3.precio, casaOeste4.precio, casaOeste5.precio);
+
+
--- codigo.js
+++ codigo.js
@@ -1,235 +1,110 @@
-let nombre = prompt("Ingrese su nombre");
 let contenedorCasa = document.getElementById("listaCasas")
+let cantidad = document.getElementById("cant");
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+cantidad.innerText = `🛒${carrito.length}`;
 
-for (let i = 1; i <= 2; i++)
-    if (nombre == "") {
-        alert("No ingresaste el nombre");
-        nombre = prompt("Ingrese su nombre");
-    } else {
-        continue;
+if (carrito.length != 0) {
+    for (const casa of carrito) {
+        document.getElementById("tablabody").innerHTML += `
+            <tr>
+                <td>${casa.id}</td>
+                <td>${casa.tipo}</td>
+                <td>${casa.precio}</td>
+                <td>${casa.zona}</td>
+                <td>${casa.agregado}</td>
+            </tr>
+        `;
     }
+    //incrementar el total
+    let totalCarrito = carrito.reduce((acumulador, casa) => acumulador + casa.precio, 0);
+    document.getElementById("total").innerText = "Presupuesto total: $" + totalCarrito;
+}
 
-let presupuestoTotal = 0;
-const iva = 1.10;
 
-let zonas = prompt("Hola " + nombre + ", en donde te gustaria adquirir una casa?\n1-Zona norte\n2-Zona sur\n3-Zona este\n4-Zona oeste\n0-Salir/Finalizar compra");
-
-while (zonas != "0") {
-    switch (zonas) {
-
-        //zona norte
-
-        case "1":
-
-            casaNorte.sort((a,b) => a.precio - b.precio);
-
-            function filtrarPorAgr(includes) {
-                if(piletaUsuario== "1"){
-                const filtrar = casaNorte.filter((casa) => casa.agregado.includes("c"));
-                return filtrar;
-                }else{
-                const filtrar2 = casaNorte.filter((casa) => casa.agregado.includes("s"));
-                return filtrar2;
-                }
-            }
-
-            let piletaUsuario = prompt("¿" + nombre + ", quiere agregar pileta?\n1-Si\n2-No");
-
-            while (piletaUsuario != 0) {
-                if (piletaUsuario==1) {
-                    alert('tu casa tendra pileta');
-                    const casasFiltradas = filtrarPorAgr(piletaUsuario);
-                    console.table(casasFiltradas);
-                    renderizarCasas(casasFiltradas);
-                }else if(piletaUsuario ==2){
-                    alert("tu casa no tendra pileta");
-                    const casasFiltradas = filtrarPorAgr(piletaUsuario);
-                    console.table(casasFiltradas);
-                    renderizarCasas(casasFiltradas);
-                }else {
-                    alert("error en el codigo");
-                    break;
-                }
-                piletaUsuario = prompt(nombre + " apriete 0 para Continuar");
-            }
-
-            function renderizarCasas(listaCasas){
-
-                for(const casa of listaCasas){
-                    contenedorCasa.innerHTML += `
-                    <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                    <h3 class="card-title">${casa.tipo}</h3>
-                    <h5 class="card-subtitle, pt/2">${casa.zona}</h5>
-                    <p class="card-text">${casa.agregado}</p>
-                    <p class="card-text">${casa.precio}</p>
-                    <a href="#" class="btn btn-dark">Comprar</a>
-                    </div>
-                </div>
-                    `;
-                }
-            }
-            break;
-
-        case "2":
-
-        casaSur.sort((a,b) => a.precio - b.precio);
-
-        function filtrarPorAgr2(includes) {
-            if(piletaUsuario2== "1"){
-            const filtrar = casaSur.filter((casa) => casa.agregado.includes("c"));
-            return filtrar;
-            }else{
-            const filtrar2 = casaSur.filter((casa) => casa.agregado.includes("s"));
-            return filtrar2;
-            }
-        }
-
-        let piletaUsuario2 = prompt("¿" + nombre + ", quiere agregar pileta?\n1-Si\n2-No");
-
-        while (piletaUsuario2 != 0) {
-            if (piletaUsuario2==1) {
-                alert('tu casa tendra pileta');
-                const casasFiltradas2 = filtrarPorAgr2(piletaUsuario2);
-                console.table(casasFiltradas2);
-                renderizarCasas(casasFiltradas2);
-            }else if(piletaUsuario2 ==2){
-                alert("tu casa no tendra pileta");
-                const casasFiltradas2 = filtrarPorAgr2(piletaUsuario2);
-                console.table(casasFiltradas2);
-                renderizarCasas(casasFiltradas2);
-            }else {
-                alert("error en el codigo");
-                break;
-            }
-            piletaUsuario2 = prompt(nombre + " apriete 0 para Continuar");
-        }
-
-        function renderizarCasas(listaCasas){
-
-            for(const casa of listaCasas){
-                contenedorCasa.innerHTML += `
-                <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                <h3 class="card-title">${casa.tipo}</h3>
-                <h5 class="card-subtitle, pt/2">${casa.zona}</h5>
-                <p class="card-text">${casa.agregado}</p>
-                <p class="card-text">${casa.precio}</p>
-                <a href="#" class="btn btn-dark">Comprar</a>
+function renderizarProductos(casasArray) {
+    for (const casa of casasArray) {
+        contenedorCasa.innerHTML += `
+            <div class="d-flex card col-sm-4 col-lg-3 gap-2">
+                <div class="card-body text-center">
+                    <h5 class="card-title">${casa.id}</h5>
+                    <p class="card-text">${casa.tipo}</p>
+                    <p class="card-text">$ ${casa.precio}</p>
+                    <p class="card-text"> ${casa.zona}</p>
+                    <p class="card-text"> ${casa.agregado}</p>
+                    <button id='btn${casa.id}' class="btn btn-success align-bottom">Comprar</button>
                 </div>
             </div>
-                `;
-            }
-        }
-        break;
-        //zona este
-
-        case "3":
-            casaEste.sort((a,b) => a.precio - b.precio);
-            function filtrarPorAgr3(includes) {
-                if(piletaUsuario3 == "1"){
-                const filtrar = casaEste.filter((casa) => casa.agregado.includes("c"));
-                return filtrar;
-                }else{
-                const filtrar2 = casaEste.filter((casa) => casa.agregado.includes("s"));
-                return filtrar2;
-                }
-            }
-
-            let piletaUsuario3 = prompt("¿" + nombre + ", quiere agregar pileta?\n1-Si\n2-No");
-
-            while (piletaUsuario3 != 0) {
-                if (piletaUsuario3==1) {
-                    alert('tu casa tendra pileta');
-                    const casasFiltradas3 = filtrarPorAgr3(piletaUsuario3);
-                    console.table(casasFiltradas3);
-                    renderizarCasas(casasFiltradas3);
-                }else if(piletaUsuario3 ==2){
-                    alert("tu casa no tendra pileta");
-                    const casasFiltradas3 = filtrarPorAgr3(piletaUsuario3);
-                    console.table(casasFiltradas3);
-                    renderizarCasas(casasFiltradas3);
-                }else {
-                    alert("error en el codigo");
-                    break;
-                }
-                piletaUsuario3 = prompt(nombre + " apriete 0 para Continuar");
-            }
-
-            function renderizarCasas(listaCasas){
-
-                for(const casa of listaCasas){
-                    contenedorCasa.innerHTML += `
-                    <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                    <h3 class="card-title">${casa.tipo}</h3>
-                    <h5 class="card-subtitle, pt/2">${casa.zona}</h5>
-                    <p class="card-text">${casa.agregado}</p>
-                    <p class="card-text">${casa.precio}</p>
-                    <a href="#" class="btn btn-dark">Comprar</a>
-                    </div>
-                </div>
-                    `;
-                }
-            }
-            break;
-
-        //zona oeste
-
-        case "4":
-            casaOeste.sort((a,b) => a.precio - b.precio);
-            function filtrarPorAgr4(includes) {
-                if(piletaUsuario4== "1"){
-                const filtrar = casaOeste.filter((casa) => casa.agregado.includes("c"));
-                return filtrar;
-                }else{
-                const filtrar2 = casaOeste.filter((casa) => casa.agregado.includes("s"));
-                return filtrar2;
-                }
-            }
-
-            let piletaUsuario4 = prompt("¿" + nombre + ", quiere agregar pileta?\n1-Si\n2-No");
-
-            while (piletaUsuario4 != 0) {
-                if (piletaUsuario4==1) {
-                    alert('tu casa tendra pileta');
-                    const casasFiltradas4 = filtrarPorAgr4(piletaUsuario4);
-                    console.table(casasFiltradas4);
-                    renderizarCasas(casasFiltradas4);
-                }else if(piletaUsuario4 ==2){
-                    alert("tu casa no tendra pileta");
-                    const casasFiltradas4 = filtrarPorAgr4(piletaUsuario4);
-                    console.table(casasFiltradas4);
-                    renderizarCasas(casasFiltradas4);
-                }else {
-                    alert("error en el codigo");
-                    break;
-                }
-                piletaUsuario4 = prompt(nombre + " apriete 0 para Continuar");
-            }
-
-            function renderizarCasas(listaCasas){
-
-                for(const casa of listaCasas){
-                    contenedorCasa.innerHTML += `
-                    <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                    <h3 class="card-title">${casa.tipo}</h3>
-                    <h5 class="card-subtitle, pt/2">${casa.zona}</h5>
-                    <p class="card-text">${casa.agregado}</p>
-                    <p class="card-text">${casa.precio}</p>
-                    <a href="#" class="btn btn-dark">Comprar</a>
-                    </div>
-                </div>
-                    `;
-                }
-            }
-            break;
-
-        default:
-            alert("El codigo ingresado es incorrecto")
-
+        `;
     }
 
-    zonas = prompt("Hola " + nombre + ", en donde te gustaria adquirir una casa?\n1-Zona norte\n2-Zona sur\n3-Zona este\n4-Zona oeste\n0-Finalizar compra");
+casasArray.forEach((casa) => {
+    document.getElementById(`btn${casa.id}`).addEventListener("click", () => {
+        agregarACarrito(casa);
+    });
+});
+}
+
+renderizarProductos(casasArray);
+
+function agregarACarrito(casaAAgregar) {
+    carrito.push(casaAAgregar);
+    cantidad.innerText = `🛒${carrito.length}`;
+
+    Toastify({
+        text: `Agregaste ${casaAAgregar.tipo} de ${casaAAgregar.zona} al carrito`,
+        duration: 2000,
+        gravity: "top",
+        position: "left",
+        close: true,
+        style: {
+            background: "rgb(224,140,38)",
+            background: "linear-gradient(90deg, rgba(224,140,38,1) 0%, rgba(43,146,31,1) 50%, rgba(224,140,38,1) 100%)",
+        },
+    }).showToast();
+
+document.getElementById("tablabody").innerHTML += `
+<tr>
+    <td>${casaAAgregar.id}</td>
+    <td>${casaAAgregar.tipo}</td>
+    <td>${casaAAgregar.precio}</td>
+    <td>${casaAAgregar.zona}</td>
+    <td>${casaAAgregar.agregado}</td>
+</tr>
+`;
+
+let totalCarrito = carrito.reduce((acumulador, casa) => acumulador + casa.precio, 0);
+document.getElementById("total").innerText = "Presupuesto total: $" + totalCarrito;
+localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+
+let finBoton = document.getElementById("finalizar");
+let cancelarBoton = document.getElementById("cancelar")
+
+finBoton.onclick = () => {
+    Swal.fire({
+        icon: "success",
+        title: "Compra Exitosa",
+        text: "Gracias por tu compra! Un asesor se comunicara con usted",
+    })
+
+    carrito = [];
+    document.getElementById("tablabody").innerHTML = "";
+    cantidad.innerText = `🛒${carrito.length}`;
+    document.getElementById("total").innerText = "Presupuesto total: $";
+    localStorage.removeItem("carrito");
+}
+
+cancelarBoton.onclick = () => {
+    Swal.fire({
+        icon: "error",
+        title: "Se cancelo la compra",
+        text: "Usted ha cancelado el proceso de compra.",
+    })
+
+    carrito = [];
+    document.getElementById("tablabody").innerHTML = "";
+    cantidad.innerText = `🛒${carrito.length}`;
+    document.getElementById("total").innerText = "Presupuesto total: $";
+    localStorage.removeItem("carrito");
 }
